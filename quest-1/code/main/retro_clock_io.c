74eda963970486a4c9c1ce7c1e63fbc3f6a754be
Joseph Rossi <josephr@bu.edu>
2019-09-14 13:13:06 -0400

    Removing print statements.

--- quest-1/code/main/retro_clock_io.c
+++ quest-1/code/main/retro_clock_io.c
@@ -7,7 +7,6 @@ void retro_clock_io_init(retro_clock_t *clock)
 
 void retro_clock_io_update(retro_clock_t *clock)
 {
-    printf("IO Update Called.\n");
 }
 
 void retro_clock_io_main(retro_clock_t *clock)
