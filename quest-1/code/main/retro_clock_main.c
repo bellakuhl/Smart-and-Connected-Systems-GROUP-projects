5983d4379ea4e3c868499dabdf717de9edd17dcf
Joseph Rossi <josephr@bu.edu>
2019-09-18 21:35:37 -0400

    Adding delays to example to make it a bit more realistic.

--- quest-1/code/main/retro_clock_main.c
+++ quest-1/code/main/retro_clock_main.c
@@ -22,18 +22,30 @@ void example_sound_alarm()
         .seconds=0
     };
 
+
+    retro_clock_change_mode(&g_clock, RC_MODE_SET_TIME);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+
     retro_clock_set_time(&g_clock, time);
-    retro_clock_alarm_set_state(&g_clock, RC_ALARM_STATE_ENABLED);
+    retro_clock_change_mode(&g_clock, RC_MODE_CLOCK);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+
+    retro_clock_change_mode(&g_clock, RC_MODE_SET_ALARM);
     retro_clock_alarm_set_time(&g_clock, alarm_time);
+    retro_clock_alarm_set_state(&g_clock, RC_ALARM_STATE_ENABLED);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+
     retro_clock_change_mode(&g_clock, RC_MODE_CLOCK);
     vTaskDelay(10000/portTICK_PERIOD_MS);
-    retro_clock_alarm_dismiss(&g_clock);
 
+    retro_clock_alarm_dismiss(&g_clock);
     alarm_time.seconds = 10;
     retro_clock_alarm_set_time(&g_clock, alarm_time);
     vTaskDelay(10000/portTICK_PERIOD_MS);
+
     retro_clock_alarm_dismiss(&g_clock);
 }
+
 
 void app_main(void)
 {
