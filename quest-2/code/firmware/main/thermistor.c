64a50d28c73ab15cf3d9c33108944c2e61c34056
Laura Reeve <lreeve@bu.edu>
2019-10-04 12:53:21 -0400

    added no of samples

--- quest-2/code/firmware/main/thermistor.c
+++ quest-2/code/firmware/main/thermistor.c
@@ -9,6 +9,8 @@
 #include <math.h>
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   10          //Multisampling
+
 
 static esp_adc_cal_characteristics_t *adc_chars;
 static const adc_channel_t channel = ADC_CHANNEL_3;     //GPIO34 if ADC1, GPIO14 if ADC2, huzzah pin A3
