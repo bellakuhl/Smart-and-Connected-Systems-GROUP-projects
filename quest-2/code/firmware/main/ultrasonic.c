50bfbfea6c08696c268cc49801cbd5b48c1c78dc
Joseph Rossi <josephr@bu.edu>
2019-10-07 20:38:36 -0400

    Convert ultrasonic to meters.

--- quest-2/code/firmware/main/ultrasonic.c
+++ quest-2/code/firmware/main/ultrasonic.c
@@ -8,25 +8,20 @@
 #include "esp_adc_cal.h"
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES   100          //Multisampling
+#define NO_OF_SAMPLES   100         //Multisampling
 
 static esp_adc_cal_characteristics_t *adc_chars;
-static const adc_channel_t channel = ADC_CHANNEL_4;     //GPIO32 if ADC1, GPIO13 if ADC2
-static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_channel_t channel = ADC_CHANNEL_0;     //GPIO32 if ADC1, GPIO13 if ADC2
+static const adc_atten_t atten = ADC_ATTEN_DB_0;
 static const adc_unit_t unit = ADC_UNIT_1; 			//ADC1
 
 int ultrasonic_init()
 {
-    if (unit == ADC_UNIT_1) {
-        adc1_config_width(ADC_WIDTH_BIT_12);
-        adc1_config_channel_atten(channel, atten);
-    } else {
-        adc2_config_channel_atten((adc2_channel_t)channel, atten);
-    }
-
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(channel, atten);
     //Characterize ADC
     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+    esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
 
     return 0;
 }
@@ -36,18 +31,14 @@ float ultrasonic_read_meters()
 	uint32_t adc_reading = 0;
     //Multisampling
     for (int i = 0; i < NO_OF_SAMPLES; i++) {
-        if (unit == ADC_UNIT_1) {
-            adc_reading += adc1_get_raw((adc1_channel_t)channel);
-        } else {
-            int raw;
-            adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
-            adc_reading += raw;
-        }
+        adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        vTaskDelay(1/portTICK_PERIOD_MS);
     }
     adc_reading /= NO_OF_SAMPLES;
     //Convert adc_reading to voltage in mV
     uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
-    uint32_t dist = (voltage / 9.88);		// Using 5V
-    return dist;
+    printf("UV: %d -", voltage);
+    uint32_t dist = (voltage / (5500.0f/512.0f));		// Using 5V
+    return dist * 0.0254;
 }
 
