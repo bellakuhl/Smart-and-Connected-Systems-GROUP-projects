cc13b0022a81f32aec9731be2a7eebf6a5bebbd2
Laura Reeve <lreeve@bu.edu>
2019-10-22 00:53:18 -0400

    Adding in basic files for pedometer, thermistor, battery monitor

---
+++ quest-3/code/firmware/main/battery.c
@@ -0,0 +1,52 @@
+// Adapted from battery monitor skill
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/i2c.h"
+#include "sensor_central.h"
+
+#define DEFAULT_VREF    1100
+#define NO_OF_SAMPLES   10          // number of samples
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;  // GPIO34 ----> pin A2
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static const adc_unit_t unit = ADC_UNIT_1;
+
+int battery_monitor_int()
+{
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(channel, atten);
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));         // characterize ADC
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+
+    return 0;
+}
+
+float battery_monitor_read_volts()
+{
+    uint32_t adc_reading = 0;
+
+    for (int i = 0; i < NO_OF_SAMPLES; i++) {                             // get multiple samples
+        if (unit == ADC_UNIT_1) {
+            adc_reading += adc1_get_raw((adc1_channel_t)channel);
+        } else {
+            int raw;
+            adc2_get_raw((adc2_channel_t)channel, ADC_WIDTH_BIT_12, &raw);
+            adc_reading += raw;
+        }
+    }
+    adc_reading /= NO_OF_SAMPLES;                                         // average samples
+
+    float voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);   // convert adc_reading to  mV
+    voltage /= 1000;  // convert to volts
+
+    return voltage;
+}
